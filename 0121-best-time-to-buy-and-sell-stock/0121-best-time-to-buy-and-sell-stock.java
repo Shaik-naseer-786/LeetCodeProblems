959f3357189652b6d849c1a4ffb9e3b691dc15e6
Naseershaik <138489079+Shaik-naseer-786@users.noreply.github.com>
2024-06-09 14:02:18 +0530

    Time: 2 ms (73.95%), Space: 61.3 MB (73.99%) - LeetHub

--- 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.java
+++ 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.java
@@ -1,18 +1,18 @@
 class Solution {
-    public int maxProfit(int[] arr) {
-        // int maxi=0;
-        // int min=prices[0];
-        // for(int i:prices){
-        //     min=Math.min(min,i);
-        //     maxi=Math.max(maxi,i-min);
-        // }
-        // return maxi;
-        int[] dp=new int[arr.length];
-        int minPrice=arr[0];
-        for(int i=1;i<arr.length;i++){
-            minPrice=Math.min(minPrice,arr[i]);
-            dp[i]=Math.max(dp[i-1],arr[i]-minPrice);
+    public int maxProfit(int[] prices) {
+        int maxi=0;
+        int min=prices[0];
+        for(int i:prices){
+            min=Math.min(min,i);
+            maxi=Math.max(maxi,i-min);
         }
-        return dp[arr.length-1];
+        return maxi;
+        // int[] dp=new int[arr.length];
+        // int minPrice=arr[0];
+        // for(int i=1;i<arr.length;i++){
+        //     minPrice=Math.min(minPrice,arr[i]);
+        //     dp[i]=Math.max(dp[i-1],arr[i]-minPrice);
+        // }
+        // return dp[arr.length-1];
     }
 }
